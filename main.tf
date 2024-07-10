b445802b1ba943b7f62994137563e45ec9f07b63
Municipal-Parking <pkwanglin@gmail.com>
2024-07-10 12:24:56 -0400

    Terraform changes

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
-output "name" {
-    value = "hi from terraform"
-}
+provider "azurerm" {
+    features {
+      
+    }
 
-//test 2
+    //creating first resource
+    resource "azurerm_resource_group" "resource_group" {
+        name = "rg-terraform-demo"
+        location = "eastus"
+    }
+
+    resource "azurerm_storage_account" "storage_account" {
+        name = "stterraformdemo"
+        resource_group_name = azurerm_resource_group.resource_group.name
+        location = azurerm_resource_group.resource_group.location
+        account_tier = "Standard"
+        account_replication_type = "LRS"
+    }
+
+}
